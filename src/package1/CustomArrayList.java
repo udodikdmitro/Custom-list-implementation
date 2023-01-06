33df4e5d67352ca03717650d1ad36d3a14ce21fb
dima udodik <dima udodik>
2023-01-06 06:23:19 +0200

    Code was optimized.

--- src/package1/CustomArrayList.java
+++ src/package1/CustomArrayList.java
@@ -112,13 +112,10 @@ public class CustomArrayList implements List<String> {
             String[] a = Arrays.copyOf(elements, (int) (elements.length + c.size() * 1.5));
             elements = a;
         }
-        String[] newElements = new String[elements.length];
-        System.arraycopy(elements, 0, newElements, 0, index);
-        System.arraycopy(elements, index, newElements,
+        System.arraycopy(elements, index, elements,
                 index + c.size(), size - index);
         Object[] arrayC = c.toArray();
-        System.arraycopy(arrayC, 0, newElements, index, c.size());
-        elements = newElements;
+        System.arraycopy(arrayC, 0, elements, index, c.size());
         size = size + c.size();
         return true;
     }
